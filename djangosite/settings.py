f58eeb0c34551c3bca8f9e0d7c39ea7138f3f990
Anuar <126041946+brightIIbyte@users.noreply.github.com>
2023-03-23 21:34:26 +0600

    notfound

--- djangosite/settings.py
+++ djangosite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0!3r15(q2g)-#!85&z^76gb4jn8sbv3syyz_u5wtc(x-%0twm5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
--- women/views.py
+++ women/views.py
@@ -1,11 +1,17 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Страница приложения women.")
 
 def categories(request, catid):
+    if request.POST:
+        print(request.POST)
+
     return HttpResponse(f"<h1>Статьи по категориям</h1><p>{catid}</p>")
 
 def archive(request, year):
     return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound(f"<h1>Страница не найдена</h1>")

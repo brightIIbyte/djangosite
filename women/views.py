fe7add2a5ad4ac80b95f6b0c160d80654a6cbada
Anuar <126041946+brightIIbyte@users.noreply.github.com>
2023-03-23 20:12:00 +0600

    archive

--- women/views.py
+++ women/views.py
@@ -6,3 +6,6 @@ def index(request):
 
 def categories(request, catid):
     return HttpResponse(f"<h1>Статьи по категориям</h1><p>{catid}</p>")
+
+def archive(request, year):
+    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")

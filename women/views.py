c6a862edbf464718411c0666c19d2e4081ba52b9
Anuar <126041946+brightIIbyte@users.noreply.github.com>
2023-03-24 22:08:32 +0600

    home

--- women/views.py
+++ women/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse, HttpResponseNotFound
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.shortcuts import render, redirect
+
 
 def index(request):
     return HttpResponse("Страница приложения women.")
@@ -11,6 +12,10 @@ def categories(request, catid):
     return HttpResponse(f"<h1>Статьи по категориям</h1><p>{catid}</p>")
 
 def archive(request, year):
+    if int (year) > 2020:
+        return redirect('home', permanent=False)
+
+
     return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
 
 def pageNotFound(request, exception):

94ee356e63dd06d1ef2f31cffacaba1263159978
Anuar <126041946+brightIIbyte@users.noreply.github.com>
2023-04-01 20:45:48 +0600

    db

--- djangosite/settings.py
+++ djangosite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0!3r15(q2g)-#!85&z^76gb4jn8sbv3syyz_u5wtc(x-%0twm5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
